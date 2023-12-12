c894cfebb521d4da027f6dafd428e05c16f1304d
seungahdev <cnu4530@snu.ac.kr>
2023-12-12 15:18:38 +0900

    use thread::spawn()

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -8,12 +8,12 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    //`bind` returns a Result<T, E> so we need to unwrap it
-
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream); // can make infinite thread!
+        });
     }
 }
 
